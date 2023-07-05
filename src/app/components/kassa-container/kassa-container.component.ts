573bf7c56297bd13f567609c8516860b77ce0c2f
VerkerkeMartijn <61974818+VerkerkeMartijn@users.noreply.github.com>
2023-07-05 11:03:04 +0200

    update-3

--- src/app/components/kassa-container/kassa-container.component.ts
+++ src/app/components/kassa-container/kassa-container.component.ts
@@ -1,6 +1,7 @@
 import { FirebaseService } from './../../shared/firebase.service';
 import { Component } from '@angular/core';
 import { Product } from 'src/app/entities/product';
+import { arrayRemove } from 'firebase/firestore';
 
 @Component({
   selector: 'app-kassa-container',
@@ -9,6 +10,9 @@ import { Product } from 'src/app/entities/product';
 })
 export class KassaContainerComponent {
   products: Array<Product> = [];
+  totaal: Array<Product>= [];
+  
+  
 
   constructor(private firebaseService: FirebaseService) {
     this.firebaseService.getProducts()
@@ -17,6 +21,26 @@ export class KassaContainerComponent {
 
   public addProductToTab(product: Product) {
       console.log("Product: ", product);
+      this.totaal.push(product);
+      console.log(this.totaal);
+      console.log(this.products);
+      this.addProductToList();
+  }
+  public addProductToList() {
+    console.log("check de list")
+
+  }
+  public DeleteProductFromList(product: Product){
+    console.log(product)
+     const indexOfProd = this.totaal.indexOf(product,0)
+     console.log(indexOfProd);
+     this.totaal.splice(indexOfProd,1);
+
+
+
   }
 
 }
+
+
+
